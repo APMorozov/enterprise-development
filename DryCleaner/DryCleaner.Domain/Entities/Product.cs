ba1321a3f596eae485eab7d1b028c90cbbbfb3bd
APMorozov <morozov-sasha200@yandex.ru>
2026-09-16 20:46:05 +0400

    Поправил опечатку

--- DryCleaner/DryCleaner.Domain/Entities/Product.cs
+++ DryCleaner/DryCleaner.Domain/Entities/Product.cs
@@ -1,7 +1,28 @@
 ﻿namespace DryCleaner.Domain.Entities;
 
+/// <summary>
+/// Изделие 
+/// </summary>
 public class Product
 {
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
+    public required int Id { get; set; }
+
+    /// <summary>
+    /// Название изделия
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Материал изделия
+    /// </summary>
+    public required string Material { get; set; }
+
+    /// <summary>
+    /// Категория изделия 
+    /// </summary>
+    public required ProductCategory Category { get; set; }
 
 }
-
