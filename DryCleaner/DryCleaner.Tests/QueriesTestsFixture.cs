b3729bdf6ab14dc887acf67b5c9a286aa4912d79
APMorozov <morozov-sasha200@yandex.ru>
2026-09-18 10:38:55 +0400

    Описана фикстура для тестов

---
+++ DryCleaner/DryCleaner.Tests/QueriesTestsFixture.cs
@@ -0,0 +1,15 @@
+﻿using DryCleaner.Domain.Data;
+using DryCleaner.Domain.Entities;
+using DryCleaner.Domain.Enums;
+
+namespace DryCleaner.Tests;
+
+internal class QueriesTestsFixture
+{
+    public IReadOnlyList<ProductCategory> Categories { get; } = DataSeed.Categories;
+    public IReadOnlyList<Client> Clients { get; } = DataSeed.Clients;
+    public IReadOnlyList<Product> Products { get; } = DataSeed.Products;
+
+
+
+}
